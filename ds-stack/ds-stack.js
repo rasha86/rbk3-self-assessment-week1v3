2c9c301c08d389c584f5c8dcac19f7d0d1362446
rasha86 <rasha.abusamra86@gmail.com>
2017-08-26 09:54:13 +0300

    i finish the stack

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,9 +1,19 @@
-var Stack = function() {
-    this._storage =
-    this.add = function(){
-      // write me
-    };
-    this.remove = function() {
-      // write me
-    };
-  };
+var Stack = function(value) {
+	this.first = 0;
+	this.last=0;
+    this._storage = [];
+    Stack.prototype.add = function(){
+
+    	storage[this.first] = this.value;
+    	this.first++    };
+
+    Stack.prototype.remove = function() {
+    		if(this.first - this.last > 0){
+    			var x = storage[this.first - this.last-1];
+    			delete x;
+    			this.first --;
+    			return x;
+    		}
+    		else 
+    			this.last = 0;    };
+    	};
