0075023e3adf62dda858e21b37dede856830f65e
rasha86 <rasha.abusamra86@gmail.com>
2017-08-26 10:04:22 +0300

    i finish the array

--- js-array-extensions/js-array-extensions.js
+++ js-array-extensions/js-array-extensions.js
@@ -1,1 +1,21 @@
-// your code here
+function extendArray(array) {
+ var newArray=[]
+
+extend(newArray,method)
+ return newArray
+}
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+}
+
+var method= [];
+method.first= function(){
+ var first= this.array[0]
+}
+method.last= function(){
+	var index = this.array.length.
+ var last= this.array[index-1]
+}
